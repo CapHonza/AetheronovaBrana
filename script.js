0244d2c21cc8e7a1f5c211797e21400338836347
Jan Čáp <capjan.work@gmail.com>
2025-12-26 22:55:55 +0100

    Poslední úpravy hlavních stránek.

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+/* =========================================
+   ČÁST 1: HLAVNÍ BRÁNA (Index.html)
+   ========================================= */
+
+// Hodiny na index.html
 const hodinyElement = document.querySelector('.clock');
+
 if (hodinyElement) {
 
     function hodiny() {
@@ -20,55 +26,94 @@ if (hodinyElement) {
     setInterval(hodiny, 1000);
 }
 
-const quizButton = document.querySelector('#submit-quiz')
-if (quizButton) {
-    quizButton.addEventListener('click', vyhodnotitQuiz);
+// Heslo do SANCTUM
+const tlacitkoBrana = document.querySelector('.code-box button')
+if (tlacitkoBrana) {
+    tlacitkoBrana.addEventListener('click', sanctumSwitch);
+    function sanctumSwitch() {
+        const vstup = document.querySelector('.code-box input').value
+        if (vstup === "ANDROMEDA") {
+            window.location.href = "sanctum.html";
+        }
+        else {
+            tlacitkoBrana.style.backgroundColor = "rgba(255, 0, 0, 0.2)";
+            tlacitkoBrana.style.borderColor = "red";
+            tlacitkoBrana.style.boxShadow = "0 0 20px red";
+            tlacitkoBrana.style.color = "red";
+
+            tlacitkoBrana.innerText = "CHYBA";
+
+            setTimeout(function () {
+                tlacitkoBrana.style.backgroundColor = ""; // Vymazání = návrat k CSS
+                tlacitkoBrana.style.borderColor = "";
+                tlacitkoBrana.style.boxShadow = "";
+                tlacitkoBrana.style.color = "";
+                tlacitkoBrana.innerText = "ODEMKNOUT"; // Návrat textu
+            }, 1500);
+        }
+    }
 }
 
-function vyhodnotitQuiz() {
-    let o1 = document.querySelector('input[name="q1"]').value.trim().toLowerCase();
-    // o2El atd používám jako schránku s tím, že se hledá zaškrtnuté kolečko (:checked). Pokud uživatel nic nezaškrtne, tak pc hlásí Null
-    // o2 je jako čtení dopisu ze schránky. Kdyý budu chtít číst "Null" - "Nic", tak mi script spadne -> Použití ?
-    // ? = Zkrácená podmínka -> je o#El skutečný? ANO -> čtu .value; NE -> dostavám prázdný text ""
-    let o2El = document.querySelector('input[name="q2"]:checked'); // El používám jako označení Element
-    let o2 = o2El ? o2El.value : "";
-    let o3El = document.querySelector('input[name="q3"]:checked'); // El používám jako označení Element
-    let o3 = o3El ? o3El.value : "";
-    let o4El = document.querySelector('input[name="q4"]:checked'); // El používám jako označení Element
-    let o4 = o4El ? o4El.value : "";
-    let o5 = document.querySelector('input[name="q5"]').value.trim().toLowerCase();
+/* =========================================
+   ČÁST 2: ZKOUŠKA (Quiz.html)
+   ========================================= */
 
-    let skore = 0;
-    if (o1 === "ram") {
-        skore++;
-    }
-    if (o2 === "b") {
-        skore++;
-    }
-    if (o3 === "a") {
-        skore++;
-    }
-    if (o4 === "b") {
-        skore++;
-    }
-    if (o5 === "tma") {
-        skore++;
+// Vyhodnocování kvízu na quiz.html
+const quizButton = document.querySelector('#submit-quiz')
+
+if (quizButton) {
+    quizButton.addEventListener('click', vyhodnotitQuiz);
+
+    // Zavření Overlaye
+    const closeBtn = document.querySelector('.close-btn');
+    if (closeBtn) {
+        closeBtn.addEventListener('click', zavritOverlay);
     }
 
-    // Hledání Overlaye v HTML
-    const overlay = document.querySelector('#overlay-container');
-    const nadpis = document.querySelector('#overlay-nadpis');
-    const text = document.querySelector('#overlay-text');
-    const box = document.querySelector('.overlay-box');
+    function vyhodnotitQuiz() {
+        let o1 = document.querySelector('input[name="q1"]').value.trim().toLowerCase();
+        // o2El atd používám jako schránku s tím, že se hledá zaškrtnuté kolečko (:checked). Pokud uživatel nic nezaškrtne, tak pc hlásí Null
+        // o2 je jako čtení dopisu ze schránky. Kdyý budu chtít číst "Null" - "Nic", tak mi script spadne -> Použití ?
+        // ? = Zkrácená podmínka -> je o#El skutečný? ANO -> čtu .value; NE -> dostavám prázdný text ""
+        let o2El = document.querySelector('input[name="q2"]:checked'); // El používám jako označení Element
+        let o2 = o2El ? o2El.value : "";
+        let o3El = document.querySelector('input[name="q3"]:checked'); // El používám jako označení Element
+        let o3 = o3El ? o3El.value : "";
+        let o4El = document.querySelector('input[name="q4"]:checked'); // El používám jako označení Element
+        let o4 = o4El ? o4El.value : "";
+        let o5 = document.querySelector('input[name="q5"]').value.trim().toLowerCase();
 
-    box.classList.remove('win', 'lose');
+        let skore = 0;
+        if (o1 === "ram") {
+            skore++;
+        }
+        if (o2 === "b") {
+            skore++;
+        }
+        if (o3 === "a") {
+            skore++;
+        }
+        if (o4 === "b") {
+            skore++;
+        }
+        if (o5 === "tma") {
+            skore++;
+        }
 
-    // Nastavení textu podle výsledku
-    // classList win/lose je v CSS u tlačítek. Odtud si bere barvu
-    if (skore === 5) {
-        box.classList.add('win');
-        nadpis.innerText = "[ SYSTEM SYNC: 100% ]";
-        text.innerText = `Analýza dokončena. Tvůj myšlenkový vzorec odpovídá frekvenci Nexu. Jsi ten, na kterého jsme čekali. Entropie ustupuje.
+        // Hledání Overlaye v HTML
+        const overlay = document.querySelector('#overlay-container');
+        const nadpis = document.querySelector('#overlay-nadpis');
+        const text = document.querySelector('#overlay-text');
+        const box = document.querySelector('.overlay-box');
+
+        box.classList.remove('win', 'lose');
+
+        // Nastavení textu podle výsledku
+        // classList win/lose je v CSS u tlačítek. Odtud si bere barvu
+        if (skore === 5) {
+            box.classList.add('win');
+            nadpis.innerText = "[ SYSTEM SYNC: 100% ]";
+            text.innerText = `Analýza dokončena. Tvůj myšlenkový vzorec odpovídá frekvenci Nexu. Jsi ten, na kterého jsme čekali. Entropie ustupuje.
         
         Tvá mysl je klíčem.
 
@@ -76,26 +121,23 @@ function vyhodnotitQuiz() {
 
         (Vrať se k Bráně a zadej kód. Nezapomeň ho. V chaosu je paměť tvou jedinou zbraní.)`;
 
-    }
-    else {
-        box.classList.add('lose');
-        nadpis.innerText = "[ ERROR: SIGNAL LOST ]";
-        text.innerText = `Detekována inkoherence. Tvá mysl je stále zastřena šumem vnějšího světa. Spojení s Nexem nelze navázat.
+        }
+        else {
+            box.classList.add('lose');
+            nadpis.innerText = "[ ERROR: SIGNAL LOST ]";
+            text.innerText = `Detekována inkoherence. Tvá mysl je stále zastřena šumem vnějšího světa. Spojení s Nexem nelze navázat.
 
         Nezoufej, Poutníku. Čas je zde relativní.
 
         Zklidni svou mysl, přeformátuj své odpovědi a pokus se o synchronizaci znovu.`;
+        }
+
+        // Zobrazení Overlaye (přiřazení třídy active)
+        overlay.classList.add('active');
     }
 
-    // Zobrazení Overlaye (přiřazení třídy active)
-    overlay.classList.add('active');
-}
-
-// Funkce pro zavření Overlaye
-function zavritOverlay() {
-    document.querySelector('#overlay-container').classList.remove('active');
-}
-
-if (heslo === ANDROMEDA) {
-    
+    // Funkce pro zavření Overlaye
+    function zavritOverlay() {
+        document.querySelector('#overlay-container').classList.remove('active');
+    }
 }
